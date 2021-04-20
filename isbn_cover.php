9941c7d200e13aec725848235e667770cfd4e13b
Noah Johnson <noah.johnson@loupmail.com>
2021-04-20 14:13:44 -0500

    Add files via upload

---
+++ isbn_cover.php
@@ -0,0 +1,209 @@
+<?php
+
+/*
+ * Include the common utilities script
+ */
+require_once '/home/tundra/data/mzc/isbn_util.php';
+
+////////////////////////////////////////////////////////////////////////
+//                                                                    //
+//                              Functions                             //
+//                                                                    //
+////////////////////////////////////////////////////////////////////////
+
+/*
+ * Given an ISBN number, get the cover image path from the local
+ * database cache.
+ * 
+ * The given ISBN must be normalized and valid or an exception is
+ * thrown.
+ * 
+ * Exceptions are thrown if there is an error.  false is returned if the
+ * ISBN is not in the local cache or if it is but it doesn't have an
+ * associated cover image.
+ * 
+ * Parameters:
+ * 
+ *   $isbn : string - the normalized, valid ISBN to query for
+ * 
+ * Return:
+ * 
+ *   string path to the cover image file or false if not found
+ */
+function getCoverPath($isbn) {
+  
+  // Check parameter
+  if (checkISBN($isbn) !== true) {
+    throw new Exception("isbn_cover-" . strval(__LINE__));
+  }
+  
+  // Attempt to open a local database connection, and wrap in a
+  // try-finally block so database connection always closed on exit
+  $db = NULL;
+  $result = false;
+  try {
+    // Open the database with a read-only connection
+    try {
+      $db = new SQLite3(BOOKDB_PATH, SQLITE3_OPEN_READONLY);
+    } catch (Exception $e) {
+      throw new Exception("Couldn't open local database");
+    }
+    
+    // Query for the ISBN-13
+    $qr = NULL;
+    try {
+      // Query based on ISBN-10 or ISBN-13
+      if (strlen($isbn) === 13) {
+        $qr = $db->query(
+              "SELECT isbn13 FROM isbncache WHERE isbn13='$isbn'");
+        
+      } else if (strlen($isbn) === 10) {
+        $qr = $db->query(
+              "SELECT isbn13 FROM isbncache WHERE isbn10='$isbn'");
+        
+      } else {
+        // Shouldn't happen
+        throw new Exception("isbn_cover-" . strval(__LINE__));
+      }
+      if ($qr === false) {
+        $qr = NULL;
+        throw new Exception("Couldn't query for cover image");
+      }
+      
+      // Get the row as an associative array
+      $r = $qr->fetchArray(SQLITE3_ASSOC);
+      
+      // Only proceed if there was a row
+      if ($r !== false) {
+        $result = $r['isbn13'];
+        if (is_string($result) === false) {
+          throw new Exception("Wrong kind of ISBN data returned");
+        }
+      }
+    
+    } finally {
+      // Close result set if open
+      if (is_null($qr) === false) {
+        $qr->finalize();
+        $qr = NULL;
+      }
+    }
+    
+  } finally {
+    // Close database connection if open
+    if (is_null($db) !== true) {
+      $db->close();
+      unset($db);
+    }
+  }
+  
+  // If we found the database record, result currently holds the ISBN-13
+  // number, so turn it into a full file path
+  if ($result !== false) {
+    $result = BOOKDB_COVERS_DIR . '/' . $result;
+  }
+  
+  // If image file doesn't exist, set to false
+  if ($result !== false) {
+    if (is_file($result) !== true) {
+      $result = false;
+    }
+  }
+  
+  // Return result
+  return $result;
+}
+
+////////////////////////////////////////////////////////////////////////
+//                                                                    //
+//                           PHP entrypoint                           //
+//                                                                    //
+////////////////////////////////////////////////////////////////////////
+
+// We only support GET and HEAD requests
+//
+if (($_SERVER['REQUEST_METHOD'] !== 'GET') &&
+    ($_SERVER['REQUEST_METHOD'] !== 'HEAD')) {
+  http_response_code(405);
+  header('Content-Type: text/plain');
+  echo "Invalid request method!\n";
+  exit;
+}
+
+// If ISBN parameter not provided, 404 error
+//
+if (array_key_exists('isbn', $_GET) !== true) {
+  http_response_code(404);
+  header('Content-Type: text/plain');
+  echo "File not found!\n";
+  exit;
+}
+
+// Grab the ISBN number
+//
+$isbn = $_GET['isbn'];
+
+// Normalize the ISBN number
+//
+$isbn = normISBN($isbn);
+
+// If ISBN number is not valid, 404 error; this also checks the check
+// digit
+//
+if (checkISBN($isbn) !== true) {
+  http_response_code(404);
+  header('Content-Type: text/plain');
+  echo "File not found!\n";
+  exit;
+}
+
+// Try to get the cover image path from the cache
+//
+$image = NULL;
+try {
+  $image = getCoverPath($isbn);
+  
+} catch (Exception $e) {
+  http_response_code(404);
+  header('Content-Type: text/plain');
+  echo $e->getMessage() . "!\n";
+  exit;
+}
+
+// If we couldn't locate image, return 404
+//
+if ($image === false) {
+  http_response_code(404);
+  header('Content-Type: text/plain');
+  echo "Couldn't find cover image!\n";
+  exit;
+}
+
+// Figure out the type of image
+$itype = exif_imagetype($image);
+if ($itype === false) {
+  http_response_code(500);
+  header('Content-Type: text/plain');
+  echo "Can't determine image file type!";
+  exit;
+}
+
+// We only support GIF, JPEG, PNG, WEBP
+if (($itype !== IMAGETYPE_GIF) &&
+    ($itype !== IMAGETYPE_JPEG) &&
+    ($itype !== IMAGETYPE_PNG) &&
+     ($itype !== IMAGETYPE_WEBP)) {
+  http_response_code(500);
+  header('Content-Type: text/plain');
+  echo "Unsupported image type!";
+  exit;
+}
+  
+// Get the image MIME type
+$mtype = image_type_to_mime_type($itype);
+  
+// Write the content type header
+header("Content-Type: $mtype");
+  
+// Write the image file to output
+readfile($image);
---
+++ isbn_util.php
@@ -0,0 +1,309 @@
+<?php
+
+/*
+ * If this script was invoked directly by a client browser, return a 404
+ * error to hide it.
+ * 
+ * This script may only be used when included from other PHP scripts.
+ */
+if (__FILE__ === $_SERVER['SCRIPT_FILENAME']) {
+  http_response_code(404);
+  header('Content-Type: text/plain');
+  echo "Error 404: Not Found\n";
+  exit;
+}
+
+////////////////////////////////////////////////////////////////////////
+//                                                                    //
+//                      Configuration constants                       //
+//                                                                    //
+////////////////////////////////////////////////////////////////////////
+
+/*
+ * The file name of the main entry form.
+ * 
+ * This file should be in the same directory as the other scripts.
+ */
+const MAINFORM_FILE_NAME = 'isbn.html';
+
+/*
+ * The full path on the server to the SQLite database for the books.
+ */
+const BOOKDB_PATH = '/home/tundra/data/mzc/books.sqlite';
+
+/*
+ * The full path on the server to the directory holding cover image
+ * files where the title is the ISBN-13 and there is no extension.
+ * 
+ * Do NOT include the trailing slash on this directory name.
+ */
+const BOOKDB_COVERS_DIR = '/home/tundra/data/mzc/covers';
+
+////////////////////////////////////////////////////////////////////////
+//                                                                    //
+//                              Functions                             //
+//                                                                    //
+////////////////////////////////////////////////////////////////////////
+
+/*
+ * Send a redirect to the client so that they go to the main ISBN input
+ * form.
+ * 
+ * For this to work properly, the script must not yet have generated any
+ * output.
+ * 
+ * This function will not return.
+ * 
+ * Return:
+ * 
+ *   this function does not return!
+ */
+function redirectToForm() {
+  
+  // Determine if HTTPS was used for this request
+  $uses_https = false;
+  if (isset($_SERVER['HTTPS'])) {
+    $uses_https = true;
+  }
+  
+  // Determine protocol
+  $prot = 'http://';
+  if ($uses_https) {
+    $prot = 'https://';
+  }
+  
+  // Get the server name and script name
+  $server_name = $_SERVER['SERVER_NAME'];
+  $script_name = $_SERVER['SCRIPT_NAME'];
+  
+  // Get the index of the last / in the script name
+  $last_sep = strrpos($script_name, '/');
+  
+  // If there is a / in the script name, the parent directory is
+  // everything up to and including it; else, the parent directory is /
+  $parent_dir = NULL;
+  if ($last_sep !== false) {
+    // Parent directory is everything up to and including last separator
+    $parent_dir = substr($script_name, 0, $last_sep + 1);
+    
+  } else {
+    // Didn't find any / so parent directory is root
+    $parent_dir = '/';
+  }
+  
+  // Form the path to the form
+  $form_path = $prot . $server_name . $parent_dir . MAINFORM_FILE_NAME;
+  
+  // Redirect to form
+  http_response_code(302);
+  header("Location: $form_path");
+  exit;
+}
+
+/*
+ * Given an ISBN string, normalize the string so that it only contains
+ * the relevant digits.
+ * 
+ * This function drops all ASCII whitespace characters (tab, space,
+ * carriage return, line feed) and all ASCII characters that are not
+ * alphanumeric.
+ * 
+ * It also converts all ASCII letters to uppercase.  Note that ISBN-10
+ * numbers may have an "X" as their check digit!
+ * 
+ * This function does NOT guarantee that the value it returns is a valid
+ * ISBN.  You must use checkISBN for that.
+ * 
+ * Passing a non-string as the parameter is equivalent to passing an
+ * empty string.
+ * 
+ * Parameters:
+ * 
+ *   $str : string | mixed - the ISBN number string to normalize
+ * 
+ * Return:
+ * 
+ *   the normalized ISBN string, which is NOT guaranteed to be valid
+ */
+function normISBN($str) {
+
+  // If non-string passed, replace with empty string
+  if (is_string($str) !== true) {
+    $str = '';
+  }
+
+  // Begin with empty result
+  $isbn = '';
+  
+  // Go through each character of string
+  $slen = strlen($str);
+  for($i = 0; $i < $slen; $i++) {
+    
+    // Get current character code
+    $c = ord($str[$i]);
+    
+    // Handle based on character type
+    if (($c >= ord('a')) && ($c <= ord('z'))) {
+      // Lowercase letter, so transfer uppercase to normalized isbn
+      $isbn = $isbn . chr($c - 0x20);
+    
+    } else if (($c >= ord('A')) && ($c <= ord('Z'))) {
+      // Uppercase letter, so transfer to normalized isbn
+      $isbn = $isbn . chr($c);
+    
+    } else if (($c >= ord('0')) && ($c <= ord('9'))) {
+      // Digit, so transfer to new isbn
+      $isbn = $isbn . chr($c);
+    
+    } else if (($c >= 0x21) && ($c <= 0x7e)) {
+      // Non-alphanumeric symbol, so don't transfer
+      continue;
+    
+    } else if (($c === ord("\t")) || ($c === ord("\r")) ||
+                ($c === ord("\n")) || ($c === ord(' '))) {
+      // Whitespace, so don't transfer
+      continue;
+    
+    } else {
+      // Control or extended character so transter to normalized
+      $isbn = $isbn . chr($c);
+    }
+  }
+  
+  // Return normalized string
+  return $isbn;
+}
+
+/*
+ * Check whether the given normalized ISBN is valid.
+ * 
+ * You must first normalize the ISBN with normISBN() before passing to
+ * this function or non-normalized forms will fail validation.
+ * 
+ * To be valid, the given variable must be a string that is either 10
+ * characters or 13 characters long.
+ * 
+ * Furthermore, if it is 10 characters, then the first 9 characters must
+ * be decimal digits and the last character may either be a decimal
+ * digit or uppercase letter X.
+ * 
+ * If it is 13 characters, then all 13 characters must decimal digits.
+ * 
+ * Finally, this function extracts the last digit, which is the check
+ * digit, and uses it to check for small typos.  If the check digit is
+ * not correct, validation will fail.
+ * 
+ * Parameters:
+ * 
+ *   $str : string | mixed - the normalized ISBN string to validate
+ * 
+ * Return:
+ * 
+ *   true if valid, false if not
+ */
+function checkISBN($str) {
+  
+  // If not a string, fail validation
+  if (is_string($str) !== true) {
+    return false;
+  }
+  
+  // Get length and check that it is either 10 or 13
+  $slen = strlen($str);
+  if (($slen !== 10) && ($slen !== 13)) {
+    return false;
+  }
+  
+  // Result starts out true
+  $result = true;
+  
+  // Check that digits are valid
+  for($x = 0; $x < $slen; $x++) {
+    
+    // Get current character code
+    $c = ord($str[$x]);
+    
+    // If this is last character AND 10-digit ISBN AND last character is
+    // uppercase X, then allow it
+    if (($x >= $slen - 1) && ($slen === 10) && ($c === ord('X'))) {
+      continue;
+    }
+    
+    // Otherwise, character must be a decimal digit
+    if (($c < ord('0')) || ($c > ord('9'))) {
+      $result = false;
+      break;;
+    }
+  }
+  
+  // If we didn't pass the format check, fail
+  if ($result !== true) {
+    return false;
+  }
+  
+  // If we got here, do check digit verification depending on type of
+  // ISBN
+  if ($slen === 10) {
+    // ISBN-10 number, so get the check digit value with X used for a
+    // check value of 10
+    $checkv = ord($str[9]);
+    if (($checkv >= ord('0')) && ($checkv <= ord('9'))) {
+      $checkv = $checkv - ord('0');
+    } else if ($checkv === ord('X')) {
+      $checkv = 10;
+    } else {
+      // Shouldn't happen
+      throw new Exception("isbn_util-" . strval(__LINE__));
+    }
+    
+    // Compute the weighted sum of the non-check digits
+    $wsum = 0;
+    for($i = 0; $i < 9; $i++) {
+      $wsum = $wsum + ((10 - $i) * (ord($str[$i]) - ord('0')));
+    }
+    
+    // Add the value of the check digit to the weighted sum
+    $wsum += $checkv;
+    
+    // If weighted sum is multiple of 11, check passes; else, check
+    // fails
+    if (($wsum % 11) === 0) {
+      $result = true;
+    } else {
+      $result = false;
+    }
+    
+  } else if ($slen === 13) {
+    // ISBN-13 number, so compute the weighted sum
+    $wsum = 0;
+    for($i = 0; $i < 13; $i++) {
+      // Get current digit value
+      $d = ord($str[$i]) - ord('0');
+      
+      // If zero-based character index mod 2 is one, then weight is 3;
+      // else, it is one
+      $r = 1;
+      if (($i % 2) === 1) {
+        $r = 3;
+      }
+
+      // Update weighted sum
+      $wsum += ($r * $d);
+    }
+
+    // If weighted sum is multiple of 10, check passes; else, check
+    // fails
+    if (($wsum % 10) === 0) {
+      $result = true;
+    } else {
+      $result = false;
+    }
+    
+  } else {
+    // Shouldn't happen
+    throw new Exception("isbn_util-" . strval(__LINE__));
+  }
+  
+  // Return result
+  return $result;
+}
