a617c8f3db7e6b66d16ea6cc2046de8f4c2af626
Noah Johnson <noah.johnson@loupmail.com>
2021-04-20 15:21:06 -0500

    Update isbn.php

--- isbn.php
+++ isbn.php
@@ -5,6 +5,10 @@
  */
 require_once 'isbn_util.php';
 
+// Determine whether the barcode scanning browser is used
+//
+$barcodes = usingScanner();
+
 ?><!DOCTYPE html>
 <html lang="en">
   <head>
@@ -12,26 +16,63 @@ require_once 'isbn_util.php';
     <title>Book ISBN input</title>
     <meta name="viewport" 
         content="width=device-width, initial-scale=1.0"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+    <link
+      href="https://fonts.googleapis.com/css2?<?php echo CSS_FAMILIES;
+              ?>&display=swap"
+      rel="stylesheet"/>
+    <link href="main.css" rel="stylesheet"/>
+    <style>
+
+.scanbox {
+  margin-top: 2.5em;
+  margin-bottom: 2.5em;
+}
+
+.tctl {
+  margin-top: 1.5em;
+}
+
+.btd {
+  padding-left: 1em;
+}
+    
+    </style>
   </head>
   <body>
 
     <h1>Book ISBN input</h1>
+
+<?php if ($barcodes) { ?>
+    <p class="scanbox">
+      <a
+          href="bwstw://startscanner?field=isbn"
+          class="ctlbtn">Scan barcode</a>
+    </p>
+<?php } ?>
 
     <form
         action="<?php echo ISBNDETAIL_FILE_NAME; ?>"
         method="get"
         accept-charset="utf-8">
       <input type="hidden" name="action" id="action" value="add"/>
-      <p>
-        ISBN number:
-        <input name="isbn" id="isbn"/>
-      </p>
-      <p>
-        <input type="submit" value="Submit"/>
-      </p>
+      <table class="tctl">
+        <tr>
+          <td>ISBN number:</td>
+          <td class="btd">
+            <input name="isbn" id="isbn" class="entryctl"/>
+          </td>
+        </tr>
+      </table>
+      <table class="tctl">
+        <tr>
+          <td><input type="submit" value="Submit" class="ctlbtn"/></td>
+          <td class="btd">
+            <a class="ctlbtn" href="<?php echo BOOKLIST_FILE_NAME;
+                    ?>">Return to list</a>
+          </td>
+        </tr>
+      </table>
     </form>
-
-    <p>Return to <a href="<?php echo BOOKLIST_FILE_NAME;
-                    ?>">book list</a></p>
   </body>
 </html>
